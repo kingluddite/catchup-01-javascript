ea6f0c8ffa1a6f7280dd1b54904ad142587d3734
Kingluddite <howley.phil@gmail.com>
2020-01-15 17:22:00 -0800

    Add solution for act 4

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 var nums = [2, 4, 6, 8, 10];
 
-//use a loop to increment each element by 1
+for (var i = 0; i < nums.length; i++) {
+  nums[i] = nums[i] + 1;
+}
 
-//use a loop to print each element in the updated array
-
-/* Output should be: 
-    3
-    5
-    7
-    9
-    11
-*/
+for (var i = 0; i < nums.length; i++) {
+  console.log(nums[i]);
+}
