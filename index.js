e7bca518e77d97762b04e42b58f371b295957e94
Kingluddite <howley.phil@gmail.com>
2020-01-15 16:53:26 -0800

    Add activity 1 question

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
-console.log('###############');
-console.log('#####Simple Data Types#####');
+var fruits = ['banana', 'pear', 'apple', 'cherry', 'peach'];
+//print apple
 
-// data types
-var name = 'John Doe';
-var age = 40;
-var isSoccerFan = false;
+var favFoods = {
+  vegetable: 'brussel sprouts',
+  meat: 'tofu',
+  fruit: 'peach',
+  legume: 'peanut',
+  grain: 'millet',
+};
 
-console.log(name); // John Doe
-console.log(age); // 40
-console.log(isSoccerFan); // false
-
-console.log('###############');
-console.log('#####Complex Data Types#####');
-// more complex data types
-// array
-var friends = ['Manny', 'Moe', 'Jack'];
-console.log(friends[0]);
-
-// length of array
-console.log('The length of the friends array is ' + friends.length);
-
-// last element in an array
-console.log('The last element in the friends array is ' + friends[friends.length - 1]);
-
-// object
-var angela = {
-  name: 'Angela',
-  age: 32,
-  profession: 'Artist';
-}
-console.log(angela.profession);
+// print peanut
