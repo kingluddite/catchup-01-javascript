18ff5f55898cf611d8ea56c96d8c149bb129beef
Kingluddite <howley.phil@gmail.com>
2020-01-15 17:45:28 -0800

    Add nothing in master but comments

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-// Using the code provided and conditionals, print "High" if the provided number is over 10, "Low" if the number is under 10, and "Perfect" if the number is exactly 10.
-//
-var num = 7;
-
-// add your conditional here
-
-if (num > 10) {
-  console.log('High');
-} else if (num < 10) {
-  console.log('Low');
-} else {
-  console.log('Perfect');
-}
+// Catchup JavaScript essentials
