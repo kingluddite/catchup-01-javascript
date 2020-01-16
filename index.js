08c67f1cb8ced92bcf26220d5b2585fa3d40aa80
Kingluddite <howley.phil@gmail.com>
2020-01-15 17:14:37 -0800

    solution for activity 3

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 var metals = ['copper', 'silver', 'tin', 'gold', 'steel', 'aluminum', 'iron'];
 
-/* print the following:
-    copper
-    tin
-    steel
-    iron
-*/
+for (var i = 0; i < metals.lenth; i = i + 2) {
+  console.log(metals[i]);
+}
